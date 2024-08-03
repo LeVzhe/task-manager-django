c2b624b259ee7b160fd96dc6bebb26bbc67af465
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-08-03 19:07:24 +0300

    feat: login logic

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from tasks_app.views import index
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", index, name="index"),
     path("tasks/", include("tasks_app.urls", namespace="tasks")),
     path("users/", include("users.urls", namespace="users")),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 
 from users.views import index, UserLoginView
 
@@ -7,4 +8,5 @@ app_name = "users"  # поле обязательное
 urlpatterns = [
     path("", index, name="index"),
     path("login/", UserLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
