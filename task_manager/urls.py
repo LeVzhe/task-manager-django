bded69c2c8e42472944deb2e64f13c70c01ce03c
Siarhei Zaluzhniy <levzh93@gmail.com>
2024-08-03 22:58:48 +0300

    fix: some template fixes

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from tasks_app.views import index
 
@@ -10,3 +12,7 @@ urlpatterns = [
     path("tasks/", include("tasks_app.urls", namespace="tasks")),
     path("users/", include("users.urls", namespace="users")),
 ]
+
+if settings.DEBUG:
+    urlpatterns.append(path("__debug__", include("debug_toolbar.urls")))
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
